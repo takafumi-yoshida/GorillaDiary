957ae8d605f74181f28716d3d72b6eb1671956fd
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-01-28 15:51:28 +0900

    mater
    master

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,5 @@
+class HomesController < ApplicationController
+  def top
+    @top =  "this is top page"  
+  end
+end
---
+++ db/migrate/20200128040844_create_diaries.rb
@@ -0,0 +1,8 @@
+class CreateDiaries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :diaries do |t|
+
+      t.timestamps
+    end
+  end
+end
